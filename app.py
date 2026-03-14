033de52ed6c3f79017460a85a0891aec7168fc69
ALAGIRI1982 <alagirikanimozhi@gmail.com>
2026-03-14 20:02:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import gspread
 from google.oauth2.service_account import Credentials
 
-st.title("Faculty Course Preference System")
+st.title("Faculty Preferences for Summer 2025-2026")
 
 # -----------------------------
 # GOOGLE SHEETS CONNECTION
